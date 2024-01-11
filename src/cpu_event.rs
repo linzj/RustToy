ed552ac69da7574000884135bcd145496180f306
linzj <zuojian.lzj@alibaba-inc.com>
2024-01-11 14:12:09 +0800

    Treat PDH_CALC_NEGATIVE_DENOMINATOR as cpu shutdown, and ignore it.

--- src/cpu_event.rs
+++ src/cpu_event.rs
@@ -11,8 +11,9 @@ use windows::{
     Win32::Foundation::ERROR_SUCCESS,
     Win32::System::Performance::{
         PdhAddCounterW, PdhCollectQueryData, PdhGetFormattedCounterValue, PdhOpenQueryW,
-        PDH_CALC_NEGATIVE_VALUE, PDH_CSTATUS_NEW_DATA, PDH_CSTATUS_VALID_DATA,
-        PDH_FMT_COUNTERVALUE, PDH_FMT_DOUBLE, PDH_INVALID_ARGUMENT, PDH_INVALID_DATA,
+        PDH_CALC_NEGATIVE_DENOMINATOR, PDH_CALC_NEGATIVE_VALUE, PDH_CSTATUS_NEW_DATA,
+        PDH_CSTATUS_VALID_DATA, PDH_FMT_COUNTERVALUE, PDH_FMT_DOUBLE, PDH_INVALID_ARGUMENT,
+        PDH_INVALID_DATA,
     },
 };
 // Define an enum for the events we are interested in
@@ -107,7 +108,9 @@ impl CpuMonitor {
                         );
                         counter_handles_index += 1;
                         // This cpu has been shut down.
-                        if status == PDH_CALC_NEGATIVE_VALUE {
+                        if status == PDH_CALC_NEGATIVE_VALUE
+                            || status == PDH_CALC_NEGATIVE_DENOMINATOR
+                        {
                             continue;
                         }
 
